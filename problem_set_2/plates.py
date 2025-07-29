93af16e771d9b7f412eee6f965dbc87eb44f8fcf
Luke Ferley <Lukeferley6@gmail.com>
2025-07-29 14:44:02 +0200

    Add files via upload

---
+++ problem_set_2/plates.py
@@ -0,0 +1,42 @@
+def main():
+    plate = input("Plate: ").strip()
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(s):
+    letters=[]
+    numbers=[]
+
+    #bool value to check if a number occurs at the end of a string
+    number=False
+    if s[0].isalpha() and 2 <= len(s) <=6 and s.isupper():
+
+        #check string forrmat i.e ABC123
+        for char in s:
+            if char.isdigit():
+                number=True
+                numbers.append(char)
+            elif char.isalpha():
+                if number:
+                    return False
+                letters.append(char)
+            else:
+                return False
+
+        if numbers:
+            if numbers[0] =='0':
+                return False
+            else:
+                return True 
+        elif len(letters) < 2:
+            return False
+        else:
+            return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    main()
+
