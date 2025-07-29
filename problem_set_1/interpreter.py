33eb503178088435fdb2e35d63be31bb28a665ec
Luke Ferley <Lukeferley6@gmail.com>
2025-07-29 08:35:40 +0200

    Update interpreter.py

--- problem_set_1/interpreter.py
+++ problem_set_1/interpreter.py
@@ -1,41 +1,4 @@
-
-
-
-# def math():
-
-#     user_expression= input('Expression: ').strip()
-#     operation= ['+', '-', '*','x', '/']
-#     user_expression= user_expression.replace(' ', '')
-
-#     for i in range(1,len(user_expression)):
-#         if user_expression[i] in operation:
-#             z= user_expression[i]
-#             x_str= user_expression[:i]
-#             y_str= user_expression[i+1:]
-#             break
-#         else:
-#             return 'Invalid expression'
-
-#     try:
-#         x= float(x_str)
-#         y= float(y_str)
-#     except ValueError:
-#         return 'Invalid expression'
-    
-#     if z == '+':
-#         return x + y    
-#     elif z == '-':
-#         return x - y
-#     elif z == '*' or z == 'x':
-#         return x * y
-#     elif z == '/':
-#         if y != 0:
-#             return x / y
-#         else:
-#             return 'Error'
-# print(math())
-    
-        
+  
 def math():
     user_expression = input('Expression: ').strip()
     operation = ['+', '-', '*', 'x', '/']
