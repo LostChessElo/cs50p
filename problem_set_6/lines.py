9bb84b2a1297091aa65b82ea29aee859a44563ca
Luke Ferley <Lukeferley6@gmail.com>
2025-08-19 17:05:40 +0200

    Create lines.py

---
+++ problem_set_6/lines.py
@@ -0,0 +1,35 @@
+import sys
+
+
+def main():
+    try:
+        file_name = args()
+        count_lines = 0
+        with open(file_name, 'r') as file:
+            for line in file:
+                if check_line(line.strip()):
+                    count_lines += 1
+            print(count_lines)
+
+    except FileNotFoundError:
+        sys.exit('File does not exist')
+
+
+def args():
+    if len(sys.argv) != 2:
+        sys.exit('Too few command-line arguments' if len(sys.argv) < 2 else 'Too many command-line arguments')
+    if sys.argv[1].endswith('.py'):
+        return sys.argv[1]
+    else:
+        sys.exit('Not a Python file')
+
+
+def check_line(s):
+    if s.startswith('#') or len(s) == 0:
+        return False
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    main()
