6f5bde412751ec1ee5bfa3facea92d19dc48a3c7
Luke Ferley <Lukeferley6@gmail.com>
2025-09-12 08:16:23 +0200

    Update pizza.py

--- problem_set_6/pizza.py
+++ problem_set_6/pizza.py
@@ -4,26 +4,12 @@ from tabulate import tabulate
 
 
 def main():
-    try:
-        file_ = args()
-        headers = get_header(file_)
-        table = []  # empty 2d array
-        with open(file_) as file:
-            for line in file:
-                if line.rstrip() not in headers:
-                    item, small, large = line.rstrip().split(',')
-                    table.append([item, small, large])
+    file_ = args()
+    print(table(file_))
 
-            print(tabulate(table, headers.split(','), tablefmt="grid"))
-
-    except FileNotFoundError:
-        sys.exit('File does not exist')
-
-#check command-line arguments
 def args():
     if len(sys.argv) != 2:
-        sys.exit('Too few command-line arguments' if len(sys.argv) < 2 
-                 else 'Too many command-line arguments')
+        sys.exit('Too few command-line arguments' if len(sys.argv) < 2 else 'Too many command-line arguments')
     if sys.argv[1].endswith('.csv'):
         return sys.argv[1]
     else:
@@ -37,10 +23,25 @@ def get_header(f):
             header = ','.join(next(reader))
             return header
     except FileNotFoundError:
+        sys.exit('FIle does not exist')
+
+
+def table(file_):
+    try:
+        file_ = args()
+        headers = get_header(file_)
+        table = []  # empty 2d array
+        with open(file_) as file:
+            for line in file:
+                if line.rstrip() not in headers:
+                    item, small, large = line.rstrip().split(',')
+                    table.append([item, small, large])
+
+            return tabulate(table, headers.split(','), tablefmt="grid")
+
+    except FileNotFoundError:
         sys.exit('File does not exist')
 
 
 if __name__ == '__main__':
     main()
-
-
